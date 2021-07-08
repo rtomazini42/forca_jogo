4bc1ce0368513d7bf8236eed929411f70885cb21
renantomazini@gmail.com <renantomazini@gmail.com>
2021-07-08 11:27:37 -0300

    Refatoração básica

--- jogo.py
+++ jogo.py
@@ -9,19 +9,13 @@ def jogar():
       palavras.append(linha)
   n_aleatorio = random.randrange(0,len(palavras))
   palavra_secreta = palavras[n_aleatorio].upper()
-  #palavras = ['Torrada','Tucano','LapTop','Python','Brasil']
-  #random.shuffle(palavras)
-  #palavra_secreta = random.choice(palavras)
-  #palavra_secreta = 'Torrada'#.upper()
   letras_acertadas = []
-  #acertou = None
   erros = 0
   enforcou = False
   acertou = False
   ganho = False
   for letra in palavra_secreta:
       letras_acertadas.append('_ ')
-  #print(letras_acertadas)
   
   def imprimir():
     for l in letras_acertadas:
@@ -30,28 +24,20 @@ def jogar():
   imprimir()
   
   while enforcou == False and ganho == False:
-    #print('jogando')
     chute = input("\nDigite a letra:\n")
     chute = chute.strip()
-    #chute = chute.upper()
     index = 0
     for letra in palavra_secreta:
       if chute.upper() == letra.upper():
         acertou = True
         letras_acertadas[index] = letra
-        #print(chute)
         print('\nletra {} na posição {} \n'.format(letra,(index+1)))
         imprimir()
-        #for l in letras_acertadas:
-        #  print(l, end = '')
-      index = index + 1
-      
+      index = index + 1    
     if not '_ ' in letras_acertadas:
       print('\n\nVocê ganhou\n\n')
       print('Sua pontuação foi {}'.format(10 - erros))
       break
-      
-      
     if acertou == False:
       erros = erros + 1
       print('Você errou\n')
@@ -59,15 +45,13 @@ def jogar():
       print(8 - erros)
       print('tentativas\n\n')
       imprimir()
-      #print(letras_acertadas)
       if erros == 8:
         print('\nFim de jogo \n\n\n')
         print('a palavra era:')
         print(palavra_secreta, end='\n\n\n')
         enforcou = True
     acertou = False 
-      #if acertou == False:
-        #print('voce errou')
+
       
   banco.close()
 if __name__ == '__main__':
